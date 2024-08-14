39160371ff660c3da7ea1b63a1d68feca1bf5020
jlsgodev <jlsgo.dev@outlook.com>
2024-08-14 19:52:49 -0300

    Remover .idea e tempCodeRunnerFile.py do repositório

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-#  25. Múltiplos de um Número
- #   - **Descrição:** Crie um programa que encontre todos os múltiplos de um número fornecido
-# pelo usuário até um valor máximo também fornecido pelo usuário.
-
-numero = int(input("Digite um número inteiro positivo: "))
-maximo = int(input("Digite um valor máximo: "))
-for i in range(1,maximo+1):
-    if i % numero == 0:
-        print(i)
